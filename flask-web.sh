45f935c83e1423d6cbad4f8893c951fc99933e3c
la1den <lowfhon@outlook.com>
2023-06-06 22:38:17 +0800

    ci: add run option

--- flask-web.sh
+++ flask-web.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
 build () {
-    pip install -r requirements.txt
+    echo "Starting build..."
+    dir_name=venv
+    python -m venv $dir_name
+    if [[ -d $dir_name ]]; then
+        if source venv/bin/activate; then
+            pip install -r requirements.txt
+        else
+            echo "activate virtual environment '$dir_name'" >&2
+            exit 1
+        fi
+    else
+        echo "no such directory: '$dir_name'" >&2
+        exit 1
+    fi
     return 
 }
 
 
 main () {
     echo "
-Choose an action you want:
-1) build
-0) exit
+    Choose an action you want:
+    1) build
+    2) run
+    0) exit
     "
 
-    read -p "Enter selection [number] > "
-    if [[ $REPLY =~ ^[0-9]+$ ]]; then
-        if [[ $REPLY == 0 ]]; then
+    read -p "Enter selection [number]: "
+    case $REPLY in
+        1)
+            build
+            exit 0
+            ;;
+        2)
+            source venv/bin/activate
+            python -m flask run --debug
+            ;;
+        0)
             echo "Program terminated."
-            exit
-        fi
-        if [[ $REPLY == 1 ]]; then
-            echo "pip install -r requirements.txt"
-            # pip install -r requirements.txt
-            exit
-        fi
-    else
-        echo "Invalid entry." >&2
-        exit 1
-    fi
+            exit 1
+            ;;
+        *)
+            echo "Invalid entry." >&2
+            exit 1
+            ;;
+    esac
 }
 
 main
